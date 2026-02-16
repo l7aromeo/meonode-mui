da952d4e2fabd2e274cfcbe535f0c7280b16ac1f
L A Romeo <ukasyahrz@outlook.com>
2026-02-17 00:45:13 +0700

    feat(mui): add new component wrappers and improve formatting
    - Add node wrappers for the following MUI components:
      - `ClickAwayListener`
      - `GlobalStyles`
      - `InitColorSchemeScript`
      - `NoSsr`
      - `TablePaginationActions`
    - Improve code formatting in `mui.core.ts` by grouping related component exports.

--- src/lib/mui.core.ts
+++ src/lib/mui.core.ts
@@ -28,6 +28,7 @@ import {
   Checkbox as MuiCheckbox,
   Chip as MuiChip,
   CircularProgress as MuiCircularProgress,
+  ClickAwayListener as MuiClickAwayListener,
   Collapse as MuiCollapse,
   Container as MuiContainer,
   CssBaseline as MuiCssBaseline,
@@ -46,6 +47,7 @@ import {
   FormGroup as MuiFormGroup,
   FormHelperText as MuiFormHelperText,
   FormLabel as MuiFormLabel,
+  GlobalStyles as MuiGlobalStyles,
   Grid as MuiGrid,
   Grow as MuiGrow,
   Icon as MuiIcon,
@@ -53,6 +55,7 @@ import {
   ImageList as MuiImageList,
   ImageListItem as MuiImageListItem,
   ImageListItemBar as MuiImageListItemBar,
+  InitColorSchemeScript as MuiInitColorSchemeScript,
   Input as MuiInput,
   InputAdornment as MuiInputAdornment,
   InputBase as MuiInputBase,
@@ -72,6 +75,7 @@ import {
   MobileStepper as MuiMobileStepper,
   Modal as MuiModal,
   NativeSelect as MuiNativeSelect,
+  NoSsr as MuiNoSsr,
   OutlinedInput as MuiOutlinedInput,
   Pagination as MuiPagination,
   PaginationItem as MuiPaginationItem,
@@ -112,6 +116,7 @@ import {
   TableFooter as MuiTableFooter,
   TableHead as MuiTableHead,
   TablePagination as MuiTablePagination,
+  TablePaginationActions as MuiTablePaginationActions,
   TableRow as MuiTableRow,
   TableSortLabel as MuiTableSortLabel,
   Tabs as MuiTabs,
@@ -136,65 +141,104 @@ import { BoxTypeMap } from '@mui/system'
  * - Theme context access and inheritance
  * - Proper component validation and error handling
  */
+
 export const Accordion = createMuiNode(MuiAccordion)
 export const AccordionActions = createMuiNode(MuiAccordionActions)
 export const AccordionDetails = createMuiNode(MuiAccordionDetails)
 export const AccordionSummary = createMuiNode(MuiAccordionSummary)
+
 export const Alert = createMuiNode(MuiAlert)
 export const AlertTitle = createMuiNode(MuiAlertTitle)
+
 export const AppBar = createMuiNode(MuiAppBar)
+
 export const Autocomplete = createMuiNode(MuiAutocomplete)
+
 export const Avatar = createMuiNode(MuiAvatar)
 export const AvatarGroup = createMuiNode(MuiAvatarGroup)
+
 export const Backdrop = createMuiNode(MuiBackdrop)
+
 export const Badge = createMuiNode(MuiBadge)
+
 export const BottomNavigation = createMuiNode(MuiBottomNavigation)
 export const BottomNavigationAction = createMuiNode(MuiBottomNavigationAction)
+
 export const Box = createMuiNode(MuiBox as OverridableComponent<BoxTypeMap>)
+
 export const Breadcrumbs = createMuiNode(MuiBreadcrumbs)
+
 export const Button = createMuiNode(MuiButton)
 export const ButtonBase = createMuiNode(MuiButtonBase)
 export const ButtonGroup = createMuiNode(MuiButtonGroup)
+
 export const Card = createMuiNode(MuiCard)
 export const CardActionArea = createMuiNode(MuiCardActionArea)
 export const CardActions = createMuiNode(MuiCardActions)
 export const CardContent = createMuiNode(MuiCardContent)
 export const CardHeader = createMuiNode(MuiCardHeader)
 export const CardMedia = createMuiNode(MuiCardMedia)
+
 export const Checkbox = createMuiNode(MuiCheckbox)
+
 export const Chip = createMuiNode(MuiChip)
+
 export const CircularProgress = createMuiNode(MuiCircularProgress)
+
+export const ClickAwayListener = createMuiNode(MuiClickAwayListener)
+
 export const Collapse = createMuiNode(MuiCollapse)
+
 export const Container = createMuiNode(MuiContainer)
+
 export const CssBaseline = createMuiNode(MuiCssBaseline)
+
 export const Dialog = createMuiNode(MuiDialog)
 export const DialogActions = createMuiNode(MuiDialogActions)
 export const DialogContent = createMuiNode(MuiDialogContent)
 export const DialogContentText = createMuiNode(MuiDialogContentText)
 export const DialogTitle = createMuiNode(MuiDialogTitle)
+
 export const Divider = createMuiNode(MuiDivider)
+
 export const Drawer = createMuiNode(MuiDrawer)
+
 export const Fab = createMuiNode(MuiFab)
+
 export const Fade = createMuiNode(MuiFade)
+
 export const FilledInput = createMuiNode(MuiFilledInput)
+
 export const FormControl = createMuiNode(MuiFormControl)
 export const FormControlLabel = createMuiNode(MuiFormControlLabel)
 export const FormGroup = createMuiNode(MuiFormGroup)
 export const FormHelperText = createMuiNode(MuiFormHelperText)
 export const FormLabel = createMuiNode(MuiFormLabel)
+
+export const GlobalStyles = createMuiNode(MuiGlobalStyles)
+
 export const Grid = createMuiNode(MuiGrid)
+
 export const Grow = createMuiNode(MuiGrow)
+
 export const Icon = createMuiNode(MuiIcon)
 export const IconButton = createMuiNode(MuiIconButton)
+
 export const ImageList = createMuiNode(MuiImageList)
 export const ImageListItem = createMuiNode(MuiImageListItem)
 export const ImageListItemBar = createMuiNode(MuiImageListItemBar)
+
+export const InitColorSchemeScript = createMuiNode(MuiInitColorSchemeScript)
+
 export const Input = createMuiNode(MuiInput)
 export const InputAdornment = createMuiNode(MuiInputAdornment)
 export const InputBase = createMuiNode(MuiInputBase)
 export const InputLabel = createMuiNode(MuiInputLabel)
+
 export const LinearProgress = createMuiNode(MuiLinearProgress)
+
 export const Link = createMuiNode(MuiLink)
+
 export const List = createMuiNode(MuiList)
 export const ListItem = createMuiNode(MuiListItem)
 export const ListItemAvatar = createMuiNode(MuiListItemAvatar)
@@ -202,33 +246,56 @@ export const ListItemButton = createMuiNode(MuiListItemButton)
 export const ListItemIcon = createMuiNode(MuiListItemIcon)
 export const ListItemText = createMuiNode(MuiListItemText)
 export const ListSubheader = createMuiNode(MuiListSubheader)
+
 export const Menu = createMuiNode(MuiMenu)
 export const MenuItem = createMuiNode(MuiMenuItem)
 export const MenuList = createMuiNode(MuiMenuList)
+
 export const MobileStepper = createMuiNode(MuiMobileStepper)
+
 export const Modal = createMuiNode(MuiModal)
+
 export const NativeSelect = createMuiNode(MuiNativeSelect)
+
+export const NoSsr = createMuiNode(MuiNoSsr)
+
 export const OutlinedInput = createMuiNode(MuiOutlinedInput)
+
 export const Pagination = createMuiNode(MuiPagination)
 export const PaginationItem = createMuiNode(MuiPaginationItem)
+
 export const Paper = createMuiNode(MuiPaper)
+
 export const Popover = createMuiNode(MuiPopover)
+
 export const Popper = createMuiNode(MuiPopper)
+
 export const Portal = createMuiNode(MuiPortal)
+
 export const Radio = createMuiNode(MuiRadio)
 export const RadioGroup = createMuiNode(MuiRadioGroup)
+
 export const Rating = createMuiNode(MuiRating)
+
 export const ScopedCssBaseline = createMuiNode(MuiScopedCssBaseline)
+
 export const Select = createMuiNode(MuiSelect)
+
 export const Skeleton = createMuiNode(MuiSkeleton)
+
 export const Slide = createMuiNode(MuiSlide)
+
 export const Slider = createMuiNode(MuiSlider)
+
 export const Snackbar = createMuiNode(MuiSnackbar)
 export const SnackbarContent = createMuiNode(MuiSnackbarContent)
+
 export const SpeedDial = createMuiNode(MuiSpeedDial)
 export const SpeedDialAction = createMuiNode(MuiSpeedDialAction)
 export const SpeedDialIcon = createMuiNode(MuiSpeedDialIcon)
+
 export const Stack = createMuiNode(MuiStack)
+
 export const Step = createMuiNode(MuiStep)
 export const StepButton = createMuiNode(MuiStepButton)
 export const StepConnector = createMuiNode(MuiStepConnector)
@@ -236,11 +303,17 @@ export const StepContent = createMuiNode(MuiStepContent)
 export const StepIcon = createMuiNode(MuiStepIcon)
 export const StepLabel = createMuiNode(MuiStepLabel)
 export const Stepper = createMuiNode(MuiStepper)
+
 export const SvgIcon = createMuiNode(MuiSvgIcon)
+
 export const SwipeableDrawer = createMuiNode(MuiSwipeableDrawer)
+
 export const Switch = createMuiNode(MuiSwitch)
+
 export const Tab = createMuiNode(MuiTab)
 export const TabScrollButton = createMuiNode(MuiTabScrollButton)
+export const Tabs = createMuiNode(MuiTabs)
+
 export const Table = createMuiNode(MuiTable)
 export const TableBody = createMuiNode(MuiTableBody)
 export const TableCell = createMuiNode(MuiTableCell)
@@ -248,15 +321,23 @@ export const TableContainer = createMuiNode(MuiTableContainer)
 export const TableFooter = createMuiNode(MuiTableFooter)
 export const TableHead = createMuiNode(MuiTableHead)
 export const TablePagination = createMuiNode(MuiTablePagination)
+export const TablePaginationActions = createMuiNode(MuiTablePaginationActions)
 export const TableRow = createMuiNode(MuiTableRow)
 export const TableSortLabel = createMuiNode(MuiTableSortLabel)
-export const Tabs = createMuiNode(MuiTabs)
+
 export const TextField = createMuiNode(MuiTextField)
+
 export const TextareaAutosize = createMuiNode(MuiTextareaAutosize)
+
 export const ThemeProvider = createMuiNode(MuiThemeProvider)
+
 export const ToggleButton = createMuiNode(MuiToggleButton)
 export const ToggleButtonGroup = createMuiNode(MuiToggleButtonGroup)
+
 export const Toolbar = createMuiNode(MuiToolbar)
+
 export const Tooltip = createMuiNode(MuiTooltip)
+
 export const Typography = createMuiNode(MuiTypography)
+
 export const Zoom = createMuiNode(MuiZoom)
